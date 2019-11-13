2239da7fc54946271989fb48500e3a1a7a6ab97c
mehmethizmali <mehmethizmali@gmail.com>
2019-11-13 16:20:36 +0300

    Sitede gönderi oluşturuldu ve düzenleme için viewler eklendi

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), #views altındaki post_list metodunun yoluna bakar
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
